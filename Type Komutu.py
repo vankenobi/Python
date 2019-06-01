212b2b972f19fc44b1d4b6a406330b27415afb3d
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-06-01 21:06:37 +0300

    Create Type Komutu.py

---
+++ Type Komutu.py
@@ -0,0 +1,10 @@
+>>> type([12,13,145,15])
+<class 'list'>
+>>> type((12,13,14,15))
+<class 'tuple'>
+>>> type(["merhaba",12])
+<class 'list'>
+>>> type({"merhaba","Mustafa"})
+<class 'set'>
+>>> type({"merhaba":"Musa"})
+<class 'dict'>
