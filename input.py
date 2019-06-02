51a73a681e6f0ea605e9a13d2de37b6bc3fbfc57
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-06-02 18:05:18 +0300

    Create input.py

---
+++ input.py
@@ -0,0 +1,5 @@
+a = input("Lutfen birinci degeri giriniz:")
+b = input("Lutfen ikinci degeri giriniz:")
+c = input("Lutfen ucuncu degeri giriniz:")
+
+print("girilen sayilarin toplami",int(a)+int(b)+int(c))
