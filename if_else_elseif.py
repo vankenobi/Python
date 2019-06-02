83ccbba4345fdfcd7bf1183170ddc7cf3d540a58
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-06-02 18:46:06 +0300

    Create if_else_elseif.py

---
+++ if_else_elseif.py
@@ -0,0 +1,7 @@
+a = int(input("Lutfen yasinizi giriniz:"))
+if a < 18:
+    print("Mekana giremezsiniz.")
+elif  a <= 56 : 
+    print("Mekana girebilirsiniz.")
+else:
+    print("Cok yaslisiniz giremezsiniz.")
