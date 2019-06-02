5aab4f0ef753571ebc9cadf46b266258da344efc
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-06-02 21:57:29 +0300

    Update faktoriyelhesabi.py

--- faktoriyelhesabi.py
+++ faktoriyelhesabi.py
@@ -1,4 +1,8 @@
-sayi = int(input("Lutfen faktoriyeli alinacak sayiyi giriniz:"))
-for i in range(1,sayi):
-    sayi *=i
-print(sayi)
+while(True):
+    sayi = int(input("Lutfen negatif olmayan bir sayi giriniz:"))
+    if (sayi < 0):
+        print("girdiginiz sayi negatif olamaz.")
+    else:
+        for i in range(1,sayi):
+            sayi *=i
+        print(sayi)    
