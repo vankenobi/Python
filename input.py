cff730989dca9c9bc952b44b4d50b679b4d49c51
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-06-02 18:18:37 +0300

    Update input.py

--- input.py
+++ input.py
@@ -3,3 +3,16 @@ b = input("Lutfen ikinci degeri giriniz:")
 c = input("Lutfen ucuncu degeri giriniz:")
 
 print("girilen sayilarin toplami",int(a)+int(b)+int(c))
+
+######################################################
+
+ad = input("Lutfen oyuncunun adini giriniz:")
+soyad = input("Lutfen oyuncunun soyadini giriniz:")
+numara = input("Lutfen oyuncunun numarasini giriniz:")
+
+bilgiler = [ad,soyad,numara]
+
+#ikiside aynı özelliktedir.
+
+ print("Oyuncunun adi:",ad,"Oyuncunun soyadi:",soyad,"oyuncunun numarasi:",numara)
+ print("Oyuncunun adi:{} Oyuncunun soyadi:{} Oyuncunun numarasi:{}".format(bilgiler[0],bilgiler[1],bilgiler[2]))
