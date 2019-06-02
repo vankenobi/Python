6790014100a63f9952981d5f62f15b7ec7894e30
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-06-02 20:26:18 +0300

    Update Whiledongusu_ornek_program.py

--- Whiledongusu_ornek_program.py
+++ Whiledongusu_ornek_program.py
@@ -18,3 +18,30 @@ while(True):
     elif(girkullanici != kullanici and girsifre != sifre):
        print("kullanici adi ve sifre yanlis girdiniz.")
        print("Tekrar giris yapiniz.")
+        
+        """
+        kullanici = "Musa123"
+sifre = "123456"
+
+while(True):
+
+    girkullanici = input("Kullanici adi giriniz:")
+    girsifre = input("Sifre giriniz:")
+
+    if (girkullanici == kullanici and girsifre == sifre):
+       print("Giris izni verildi.")
+       break
+    elif(girkullanici == kullanici and girsifre != sifre):
+        print("Sifreyi yanlis girdiniz.")
+        cevap = input("Sifrenizi unuttuysaniz degistirmek ister misiniz (E/H) ?:")
+        if (cevap == "E"):
+             yenisifre = input("Lutfen yeni sifre giriniz:")
+             sifre = yenisifre
+             print("Sifreniz degisti artik yeni sifreniz:",sifre)
+             break      
+        elif (cevap == "H"):
+                break    
+    elif(girkullanici != kullanici and girsifre != sifre):
+       print("kullanici adi ve sifre yanlis girdiniz.")
+       print("Tekrar giris yapiniz.")
+       """
