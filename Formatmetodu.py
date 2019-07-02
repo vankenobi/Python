e2a9b32ecc4362153494e3a360de50f8c0fdca03
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-07-02 11:46:38 +0300

    Update Formatmetodu.py

--- Formatmetodu.py
+++ Formatmetodu.py
@@ -1,3 +1,29 @@
+#OR1
+"""
 alfabe = "abcdefgğhıijklmnoöprsştuüvyz"
 for i in alfabe:
     print("Bastırılan karakter:{}".format(i))
+"""
+#OR2
+"""
+x1 = "Merhaba"
+x2 = "Nasılsın"
+
+ifade = "{1} {0}".format(x1,x2)
+print(ifade)
+"""
+#OR3
+"""
+x1 = "Merhaba"
+x2 = "Nasılsın"
+
+ifade = "{1} {0}".format(x1,x2) 
+ifade2 = "|{:^15}|".format(x1) #Texti Ortalamak için kullanılıyor.
+ifade3 = "|{:<15}|".format(x2) #Texti Sola Yaslamak için Kullanılıyor.
+ifade4 = "|{:>15}|".format(x2) #Texti Sağa Yaslamak için kullanılıyor.
+
+print(ifade)
+print(ifade2)
+print(ifade3)
+print(ifade4)
+"""
