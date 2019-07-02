8654eecc3f0cde9ecc52139d533b1ef2b817faa5
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-07-02 13:14:31 +0300

    Update Formatmetodu.py

--- Formatmetodu.py
+++ Formatmetodu.py
@@ -21,9 +21,16 @@ ifade = "{1} {0}".format(x1,x2)
 ifade2 = "|{:^15}|".format(x1) #Texti Ortalamak için kullanılıyor.
 ifade3 = "|{:<15}|".format(x2) #Texti Sola Yaslamak için Kullanılıyor.
 ifade4 = "|{:>15}|".format(x2) #Texti Sağa Yaslamak için kullanılıyor.
+ifade5 = "{:b}".format(1024) #Formatın içindeki sayıyı ikilik sistemde yazar.
+ifade6 = "{:d}".format(1023) #Formatın içine yalnızca sayısal ifade yazılır.
+ifade7 = "{:s}".format("Merhaba") #Formatın içine yalnızca String ifade girilir.
 
 print(ifade)
 print(ifade2)
 print(ifade3)
 print(ifade4)
+print(ifade5)
+print(ifade6)
+print(ifade7)
+
 """
