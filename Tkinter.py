677cd8620d1228bfc1dfda84b42504b8dce412f8
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-07-08 18:29:41 +0300

    Update Tkinter.py

--- Tkinter.py
+++ Tkinter.py
@@ -16,8 +16,17 @@ pencere.state("normal")   #pencere durumu (normal= normal bi şekilde başlar) #
 
 pencere.wm_attributes("-alpha",0.8) #Pencere saydamlığını ayarlar. örn:0 dan 1 e kadardır şuan %80 saydam
 
-yazi = Label(text = "Merhaba Dünya",fg = "red", bg = "")  # Form ekranına etiket eklemek için kullanılır.
+yazi = Label(text = "Merhaba Dünya",
+             fg = "red",
+             bg = "yellow",
+            # widht = 15, #dikey olarak yazı alanı 
+            # height = 12, #yatay olarak yazı alanı
+             padx=5,   
+             pady=15,  
+             justify("center"),  # merkezde,sola yada sağa mı yatık olacağını belirliyor.
+             anchor="se", #Yazının yazı alanındaki konumunu belirliyor.
+             font=("Lemon Milk",10,"normal",overstrike))  # Form ekranına etiket eklemek için kullanılır.
 yazi.pack() #yazi.pack yazmassak görünmez. #İkinci parametre yazi rengini belirler.üçüncü parametre yazi arkaplan rengini belirler.
-
+#4. parametre yazı fontu ile alakalı 
 
 mainloop() #pencereyi döngüye soktuk ki kapanmasın.
