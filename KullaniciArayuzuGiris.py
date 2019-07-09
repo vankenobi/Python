c505b29367320a71f0a80bbef86e757b9359f8cc
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-07-09 22:14:15 +0300

    Update KullaniciArayuzuGiris.py

--- KullaniciArayuzuGiris.py
+++ KullaniciArayuzuGiris.py
@@ -9,21 +9,23 @@ def pencere():
            durumlabel.setText("Giriş Başarılı")
        else:
            durumlabel.setText("Giriş Başarısız")
+           g1.clear()
+           g2.clear() # textbox içerisindeki yazıyı siler
 
 
    app = QApplication(sys.argv)
    window = QWidget()
-   form = QFormLayout()
+   form = QFormLayout() #Form adında bir form layout oluşturduk.
 
-   g1 = QLineEdit()
+   g1 = QLineEdit()  #g1 ve g2 adında textbox oluşturduk.
    g2 = QLineEdit()
-   g2.setEchoMode(QLineEdit.Password)
+   g2.setEchoMode(QLineEdit.Password) #textin parola şeklinde yazılmasını sağlar.
 
    durumlabel = QLabel()
    # textbox.setInputMask("+90-999-999-99-99")  Yazıyı kalıplara ayıtmak ör:tel no
    button = QPushButton(window)
    button.setText("Bağlan")
-   button.clicked.connect(giriş)
+   button.clicked.connect(giriş) #Buton basıldığında fonksiyona yönlendiriyor.
 
    form.addRow(QLabel("Kullanıcı Adı:"),g1)
    form.addRow(QLabel("Şifre:"),g2)
