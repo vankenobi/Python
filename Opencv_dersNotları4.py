384b695b937855e089e7f90fd521db90a6b2d504
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-08-07 17:08:58 +0300

    Update Opencv_dersNotları4.py

--- Opencv_dersNotları4.py
+++ Opencv_dersNotları4.py
@@ -2,10 +2,10 @@ import cv2
 import numpy as np
 
 foto = cv2.imread("Tyler.jpg")
-cv2.imshow("Tyler Reis",foto)
+cv2.imshow("Tyler",foto)
 
 cv2.rectangle(foto,(50,300),(300,50),[0,0,255],2) #Resim içerisine çerçeve dikdörtgen bir çerçeve çizmek için kullanılır.
-cv2.imshow("Tyler Reis",foto)
+cv2.imshow("Tyler",foto)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
