ee7960ba7b4109bd008887841ba1eaf44d26cece
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-08-07 16:40:41 +0300

    Update Modüller.py

--- Modüller.py
+++ Modüller.py
@@ -1,16 +1,16 @@
 #Modül1 Sayfasi
 
-def selamcak():
-    print("Naber Canım")
-def mutlakdegeral(sayi):
+def selamver():
+    print("Selam")
+def mutlak_deger_al(sayi):
     if sayi < 0:
         return -sayi
     return sayi
 
 #Modüller sayfasi
 
-from modul1 import *                     #iki çeşit modül ekleme seçeneği var.    
+from modul1 import *         #iki çeşit modül ekleme seçeneği var.    
 import modul1 
 
-modul1.selamcak()
-print(modul1.mutlakdegeral(45))
+modul1.selamver()
+print(modul1.mutlak_deger_al(45))
