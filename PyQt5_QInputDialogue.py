88e2ac31d87f02fe9d6d3ee6fc24c48505420141
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-08-08 10:25:48 +0300

    Update PyQt5_QInputDialogue.py

--- PyQt5_QInputDialogue.py
+++ PyQt5_QInputDialogue.py
@@ -10,6 +10,7 @@ class Pencere(QWidget):
         self.setUI()
     def setUI(self):
         form = QFormLayout()
+
         self.button1 = QPushButton("En sevdiğiniz dili seçin")
         self.button2 = QPushButton("İsminizi giriniz")
         self.button3 = QPushButton("Yaşınızı giriniz")
@@ -25,24 +26,27 @@ class Pencere(QWidget):
         self.button1.clicked.connect(self.dil_al)
         self.button2.clicked.connect(self.isim_al)
         self.button3.clicked.connect(self.yas_al)
+
         self.setWindowTitle("QINPUT")
         self.setLayout(form)
         self.show()
+
     def yas_al(self):
         seçilen,tamam = QInputDialog.getInt(self,"Yaş","Yaşınızı Giriniz")
         if tamam:
             self.giriş3.setText(str(seçilen))
+
     def isim_al(self):
         seçilen,tamam = QInputDialog.getText(self,"İsim","İsiminizi Giriniz")
         if tamam:
             self.giriş2.setText(str(seçilen))
+
     def dil_al(self):
         diller = ("İngilizce","Fransızca","Türkçe","Japonca","İtalyanca","Çince")
         seçilen,tamam = QInputDialog.getItem(self,"Sevdiğiniz Dil","Sevdiğiniz Dili Seçiniz",diller,0,False)    
         if tamam:
             self.giriş1.setText(str(seçilen))
         
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     pencere = Pencere()
