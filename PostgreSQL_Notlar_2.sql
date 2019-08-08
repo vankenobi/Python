db8a03f64121591b46df515e44991fb8eb01dc66
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-08-08 13:26:21 +0300

    Add files via upload

---
+++ PostgreSQL_Notlar_2.sql
@@ -0,0 +1,6 @@
+SELECT * FROM gorevliler
+WHERE boy BETWEEN 1.73 AND 1.82; # 1.73 ile 1.82 arasındaki boyu olan kişileri ekrana çıkarıcak
+###################
+SELECT * FROM gorevliler
+WHERE boy NOT BETWEEN 1.73 AND 1.82; # 1.73 ile 1.82 arasındaki boyu olan kişiler hariç diğer hepsini göstericek.
+###################
