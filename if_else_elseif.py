ebe324162bcb90d7eb4873c7db8236292adac8ba
vankenobi <43112588+vankenobi@users.noreply.github.com>
2020-06-14 20:30:47 +0300

    Delete if_else_elseif.py

--- if_else_elseif.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Lutfen yasinizi giriniz:"))
-if a < 18:
-    print("Mekana giremezsiniz.")
-elif  a <= 56 : 
-    print("Mekana girebilirsiniz.")
-else:
-    print("Cok yaslisiniz giremezsiniz.")
